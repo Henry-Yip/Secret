81e1422ad84d966c51f7277f8843106c7e17f858
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2023-06-05 20:59:54 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "No CV",
+        "title": null,
         "excerpt":"                        Protected Page                                                                                                                                                                                                          Protected Page                                                                                                                                                                                                         Remember me                                                                                                                                ","categories": [],
-        "tags": [],
+        "tags": null,
         "url": "/Journal/2023-06-05-NoCV.markdown/",
         "teaser": "/assets/images/Teaser.png"
       },]
