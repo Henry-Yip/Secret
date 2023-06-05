4f451608a09c0fd236ecd801dd66a90b6763c18d
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2023-06-05 18:57:51 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,1 @@
-var store = [{
-        "title": "Stun Locks",
-        "excerpt":"I’ve realized that most people (all) put their CVs in their personal website. Realistically I don’t see how potential future employers will stumple upon my website and even if they do, I doubt if this will affect my application procesess. Just like no one ever puts their CVs in their...","categories": [],
-        "tags": ["Journal"],
-        "url": "/Journal/NoCV/",
-        "teaser": "/assets/images/Astro.png"
-      },{
-        "title": "Private Section",
-        "excerpt":"New Private Section!  Starting from 6/6/2023, you can view the private section of my website if you have the password. I built it with the help of this guide   The Website  The Website  ","categories": [],
-        "tags": [],
-        "url": "/PrivateSection/",
-        "teaser": "/assets/images/Astro.png"
-      }]
+var store = []
