b3640ca32912ceb7513acfd4ae8a38b293511c74
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2023-06-05 19:09:37 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Burnt Down",
+        "excerpt":"&lt;!DOCTYPE html&gt;                         Protected Page                                                                                                                                                                                                          Protected Page                                                                                                                                                                                                         Remember me                                                                                                                                ","categories": [],
+        "tags": [],
+        "url": "/Journal/2023-06-04-BurntDown/",
+        "teaser": "/assets/images/Teaser.png"
+      },]
