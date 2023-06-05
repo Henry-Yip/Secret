5dbd1fa26e9c792a1d819433c135aeb3edc70ce0
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2023-06-05 17:16:57 +0800

    Update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Stun Locks",
+        "excerpt":"Stun Locks I just read Toby Lam’s post on Stun-Locks Those who know me personally in Edinburgh know I’m not a very imaginative problem solver, and my strength mainly lies in solving very arduous algebra/ differential equations with very high accuracy. So if the questions are moderately-difficult, chances are I...","categories": [],
+        "tags": ["Journal"],
+        "url": "/Journal/StunLocks/",
+        "teaser": "/assets/images/Astro.png"
+      },{
+        "title": "Stun Locks",
+        "excerpt":"I’ve realized that most people (all) put their CVs in their personal website. Realistically I don’t see how potential future employers will stumple upon my website and even if they do, I doubt if this will affect my application procesess. Just like no one ever puts their CVs in their...","categories": [],
+        "tags": ["Journal"],
+        "url": "/Journal/NoCV/",
+        "teaser": "/assets/images/Astro.png"
+      },{
+        "title": "Private Section",
+        "excerpt":"New Private Section!  Starting from 6/6/2023, you can view the private section of my website if you have the password. I built it with the help of this guide   The Website  The Website  ","categories": [],
+        "tags": [],
+        "url": "/PrivateSection/",
+        "teaser": "/assets/images/Astro.png"
+      }]
